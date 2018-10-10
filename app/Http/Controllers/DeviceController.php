1a2270c88fe07a6fb3c071d2e0091f78991d6903
kek <kek@awa.com>
2018-10-10 10:10:27 +0700

    post request for light status+db migration

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -4,23 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\light;
+//use App\AC;
+//use App\TV;
+
 class DeviceController extends Controller
 {
-    public function lightOn(){
+
+  public function __construct(light $light){
+    $this->light = $light;
+
+  }
+
+    public function lightOn(Request $request){
+      $light=[
+        "status" => $request->status
+      ];
+
       try {
-        $array = Array();
-        $array['status'] = "light on";
-        return $array;
+        $data = $this->light->create($light);
+        return response()->json($data);
       } catch (QueryException $e) {
         return response()->json(['error' => "it screwed up"], 404);
       }
     }
 
-    public function lightOff(){
+    public function lightOff(Request $request){
+      $light=[
+        "status" => $request->status
+      ];
+
       try {
-        $array = Array();
-        $array['status'] = "light off";
-        return $array;
+        $data = $this->light->create($light);
+        return response()->json($data);
       } catch (QueryException $e) {
         return response()->json(['error' => "it screwed up"], 404);
       }
